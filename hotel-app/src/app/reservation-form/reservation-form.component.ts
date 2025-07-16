e5e8385d5de874c1a98db06ee9e2092c68371f44
Jorge Lameiras <66096179+jorgelameiras@users.noreply.github.com>
2025-07-16 20:31:41 +0000

    latest change

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,11 +6,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
-
+export class ReservationFormComponent implements OnInit {
+  
   reservationForm: FormGroup = new FormGroup({});
 
-  onSubmit() {
+  // Dependency injection in Angular
+  // Learn more about this
+  constructor(private formBuilder: FormBuilder) {
     
+  }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      // This bassically creates a validation requeriment to submit the form,
+      // if the date field is empty then you wont be able to submit the form
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      // Here we are creating an array inside of another array, in order to validate
+      // if the text entered is actually an array
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit() {
+    if(this.reservationForm.valid) {
+      console.log("Valid");
+    }
+
   }
 }
